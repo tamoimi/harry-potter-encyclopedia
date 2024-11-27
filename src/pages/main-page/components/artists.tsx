d343edd49d0aeba12e2863ff337ca6dc34afdc43
tami <tami@optatumplatform.com>
2024-11-27 10:46:30 +0900

    feat: card component

---
+++ src/pages/main-page/components/artists.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Artist } from "../models/types/types";
+
+interface ArtistCardProps {
+  artist: Artist;
+}
+
+const ArtistCard: React.FC<ArtistCardProps> = ({ artist }) => {
+  return (
+    <Card key={artist.id}>
+      <CardHeader>
+        <CardTitle>{artist.name}</CardTitle>
+        <CardDescription>Genres: {artist.genres.join(", ")}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <img
+          src={artist.images[0].url}
+          alt={artist.name}
+          width={artist.images[0].width}
+          height={artist.images[0].height}
+        />
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default ArtistCard;
