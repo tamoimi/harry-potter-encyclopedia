b7e3f1b22d3e7a43c51ee584d5de7e54f54498b9
tami <tami@optatumplatform.com>
2024-11-27 15:18:04 +0900

    feat: search input

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,27 +1,42 @@
-import { useQuery } from "@tanstack/react-query";
-import { ArtistResponse } from "./models/types/types";
-import { fetchArtists } from "./libs/api/spotify";
-import ArtistCard from "./components/artists";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import ArtistCard from "./components/artists";
+import { fetchArtists } from "./libs/api/spotify";
 
 const Home = () => {
-  const { data, isLoading, isError } = useQuery<ArtistResponse>({
-    queryKey: ["artists"],
-    queryFn: fetchArtists,
+  const [searchTerm, setSearchTerm] = useState(""); // 사용자 입력 값을 저장
+  const [query, setQuery] = useState(""); // 실제 검색에 사용할 값
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["artists", query], // 검색어를 queryKey에 추가
+    queryFn: () => fetchArtists(query), // 검색어를 fetch 함수에 전달
+    enabled: !!query, // query 값이 있을 때만 실행
   });
+
+
+  const handleSearch = () => {
+    setQuery(searchTerm); // 검색 버튼 클릭 시 query 값 설정
+  };
 
   if (isLoading) return <p>Loading...</p>;
   if (isError) return <p>Error loading artists.</p>;
 
   return (
     <>
-      <div className="flex items-center w-full max-w-sm space-x-2">
-        <Input placeholder="Artist" />
-        <Button type="submit">Search</Button>
+      <div className="flex items-center w-full max-w-sm mb-10 space-x-2">
+        <Input
+          placeholder="Artist"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)} // 사용자 입력 값 업데이트
+        />
+        <Button onClick={handleSearch} type="button">
+          Search
+        </Button>
       </div>
       <div className="grid grid-cols-2 gap-4">
-        {data?.artists.map((artist) => (
+        {data?.artists.items.map((artist) => (
           <ArtistCard key={artist.id} artist={artist} />
         ))}
       </div>
